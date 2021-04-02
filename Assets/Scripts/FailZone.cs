1c99006809730d51b5b92b3203871e51b413a0a5
max-las <lasserre.maxime@yahoo.fr>
2021-04-02 16:58:58 +0200

    push salvateur

--- Assets/Scripts/FailZone.cs
+++ Assets/Scripts/FailZone.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FailZone : MonoBehaviour
 {
+
+    private Rigidbody2D ballRb2D;
 
     public GameObject ball;
     public AudioSource looseSound;
@@ -11,26 +14,33 @@ public class FailZone : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Score.lifes = 3;
+        ballRb2D = ball.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Score.lifes <= 0){
+            SceneManager.LoadScene("Menu");
+        }
     }
 
     void OnTriggerEnter2D(Collider2D other){
         if(other.gameObject.tag == "Ball"){
             IEnumerator coroutine = BallInstantiate(other);
             looseSound.Play();  
+            Score.lifes -= 1;
             StartCoroutine(coroutine);
         }
     }
 
     IEnumerator BallInstantiate(Collider2D other){
         yield return new WaitForSeconds(1);
-        Destroy(other.gameObject);
-        Instantiate(ball, new Vector2(-1, 0), Quaternion.identity);
+        ballRb2D.velocity = Vector2.zero;
+        ballRb2D.angularVelocity = 0f;
+        ballRb2D.MovePosition(new Vector2(-1, 0));
+        // Destroy(other.gameObject);
+        // Instantiate(ball, new Vector2(-1, 0), Quaternion.identity);
     }
 }
