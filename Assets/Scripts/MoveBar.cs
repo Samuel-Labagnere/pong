a7c1fbc0e7d7060ee0dd525a0584eeadf9bf21b6
max-las <lasserre.maxime@yahoo.fr>
2021-04-02 10:37:03 +0200

    barre souris

--- Assets/Scripts/MoveBar.cs
+++ Assets/Scripts/MoveBar.cs
@@ -15,8 +15,11 @@ public class MoveBar : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        Vector3 newPos = Input.mousePosition;
+        newPos.z = 10f; // Set this to be the distance you want the object to be placed in front of the camera.
+        rb2D.MovePosition(Camera.main.ScreenToWorldPoint(newPos));
 
-        Vector2 newPos = rb2D.position;
+        /*Vector2 newPos = rb2D.position;
 
         if (Input.GetAxis("Horizontal") < 0f)
         {
@@ -35,6 +38,6 @@ public class MoveBar : MonoBehaviour
             newPos += Time.fixedDeltaTime * speed * Vector2.down;
         }
 
-        rb2D.MovePosition(newPos);
+        rb2D.MovePosition(newPos);*/
     }
 }
