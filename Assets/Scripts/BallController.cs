790a9a8d95c1d80777f242d5f15c775f33325a78
The-SamL <samuel.labagnere@mmibordeaux.com>
2021-04-04 18:03:10 +0200

    LaserToc sound effect added

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -8,6 +8,7 @@ public class BallController : MonoBehaviour
     public Rigidbody2D rb2D;
     public static float speed = 500f;
     public float multiplier = 1.05f;
+    public AudioSource laserToc;
 
     // Start is called before the first frame update
     void Start()
@@ -24,6 +25,7 @@ public class BallController : MonoBehaviour
     void OnCollisionEnter2D(Collision2D other) {
         rb2D.AddForce(other.contacts[0].normal * speed);
         if(other.gameObject.tag == "Player"){
+            laserToc.Play();
             if(speed <= 10000f){
                 speed = speed * multiplier;
             }
