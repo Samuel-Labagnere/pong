740ae2ce0b787747efe69eabb65777df0b5c493e
max-las <lasserre.maxime@yahoo.fr>
2021-04-05 13:20:49 +0200

    début de remap

--- Assets/Scripts/KeyMapping.cs
+++ Assets/Scripts/KeyMapping.cs
@@ -2,9 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.InputSystem;
 
 public class KeyMapping : MonoBehaviour
 {
+    
+    private InputAction fireAction;
+    private InputAction moveAction;
+    private int up;
+
+    public PlayerInput pInput; 
 
     public InputField inputHaut;
     public InputField inputBas;
@@ -14,6 +21,11 @@ public class KeyMapping : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+
+        fireAction = pInput.actions.FindAction("Fire");
+        moveAction = pInput.actions.FindAction("Move");
+        up = moveAction.bindings.IndexOf(x => x.name == "up");
+
         InputField ih = inputHaut.GetComponent<InputField>();
         ih.onEndEdit.AddListener(delegate {Test(ih); });
     }
@@ -31,5 +43,33 @@ public class KeyMapping : MonoBehaviour
         if(input.text.Length == 0){
             Debug.Log("Test! Empty!");
         }
+    }
+
+    void Remap(){
+        Debug.Log("remap go");
+        moveAction.Disable();
+        moveAction.PerformInteractiveRebinding(up)
+            .WithControlsExcluding("Mouse")
+            .WithCancelingThrough("<Keyboard>/escape")
+            .OnMatchWaitForAnother(0.2f)
+            .Start().OnCancel(op =>
+            {
+                Debug.Log("cancel");
+ 
+            }).OnComplete(op =>
+            {
+                Debug.Log("saved");
+                moveAction.Enable();
+            });
+    }
+
+    public void Fire(InputAction.CallbackContext context)
+    {
+        Debug.Log("Fire");
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        Debug.Log("Move");
     }
 }
